e2ae479e754e52dcd91801d6a803b2a76db13f00
Anerie Patel <aneriep@gmail.com>
2023-08-14 10:19:30 -0400

    Initial Commit

---
+++ Testing with GitHub/Testing_with_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Testing_with_GitHubApp.swift
+//  Testing with GitHub
+//
+//  Created by Anerie Patel on 2023-08-14.
+//
+
+import SwiftUI
+
+@main
+struct Testing_with_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
